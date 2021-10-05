1575292ce499d9bf30df37f36b3b75cf1e17cdbf
KhaledElshamy <kh.ma205511@gmail.com>
2021-10-05 16:43:14 +0200

    Initial Commit

---
+++ NearBy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearBy
+//
+//  Created by Khaled Elshamy on 05/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
